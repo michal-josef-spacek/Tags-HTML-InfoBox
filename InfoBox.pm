17fd760511e29532fab03863a672be6cd2beb4dc
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-02-09 19:20:20 +0100

    Add doc

--- InfoBox.pm
+++ InfoBox.pm
@@ -156,3 +156,230 @@ sub _set_infobox {
 }
 
 1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Tags::HTML::InfoBox - Tags helper for HTML info box.
+
+=head1 SYNOPSIS
+
+ use Tags::HTML::InfoBox;
+
+ my $obj = Tags::HTML::InfoBox->new(%params);
+ $obj->cleanup;
+ $obj->init($infobox);
+ $obj->prepare($infobox);
+ $obj->process;
+ $obj->process_css;
+
+=head1 METHODS
+
+=head2 C<new>
+
+ my $obj = Tags::HTML::InfoBox->new(%params);
+
+Constructor.
+
+=over 8
+
+=item * C<css>
+
+L<CSS::Struct::Output> object for L<process_css> processing.
+
+Default value is undef.
+
+=item * C<css_class>
+
+Default value is 'info-box'.
+
+=item * C<lang>
+
+Language in ISO 639-1 code.
+
+Default value is undef.
+
+=item * C<tags>
+
+L<Tags::Output> object.
+
+Default value is undef.
+
+=back
+
+=head2 C<cleanup>
+
+ $obj->cleanup;
+
+Process cleanup after page run.
+
+In this case cleanup internal representation of a set by L<init>.
+
+Returns undef.
+
+=head2 C<init>
+
+ $obj->init($infobox);
+
+Process initialization in page run.
+
+Accepted C<$infobox> is L<Data::InfoBox>.
+
+Returns undef.
+
+=head2 C<prepare>
+
+ $obj->prepare($infobox);
+
+Process initialization before page run.
+
+Accepted C<$infobox> is L<Data::InfoBox>.
+
+Returns undef.
+
+=head2 C<process>
+
+ $obj->process;
+
+Process L<Tags> structure for HTML a element to output.
+
+Do nothing in case without inicialization by L<init>.
+
+Returns undef.
+
+=head2 C<process_css>
+
+ $obj->process_css;
+
+Process L<CSS::Struct> structure for HTML a element to output.
+
+Do nothing in case without inicialization by L<init>.
+
+Returns undef.
+
+=head1 ERRORS
+
+ new():
+         From Tags::HTML::new():
+                 Parameter 'css' must be a 'CSS::Struct::Output::*' class.
+                 Parameter 'tags' must be a 'Tags::Output::*' class.
+
+ init():
+         Footer object must be a 'Data::InfoBox' instance.
+
+ process():
+         From Tags::HTML::process():
+                 Parameter 'tags' isn't defined.
+
+ process_css():
+         From Tags::HTML::process_css():
+                 Parameter 'css' isn't defined.
+
+=head1 EXAMPLE
+
+=for comment filename=create_and_print_infobox.pl
+
+ use strict;
+ use warnings;
+
+ use CSS::Struct::Output::Indent;
+ use Tags::HTML::InfoBox;
+ use Tags::Output::Indent;
+ use Test::Shared::Fixture::Data::InfoBox::Street;
+ use Unicode::UTF8 qw(encode_utf8);
+
+ # Object.
+ my $css = CSS::Struct::Output::Indent->new;
+ my $tags = Tags::Output::Indent->new(
+         'xml' => 1,
+ );
+ my $obj = Tags::HTML::InfoBox->new(
+         'css' => $css,
+         'tags' => $tags,
+ );
+
+ # Data object for info box.
+ my $infobox = Test::Shared::Fixture::Data::InfoBox::Street->new;
+
+ # Initialize.
+ $obj->init($infobox);
+
+ # Process.
+ $obj->process;
+ $obj->process_css;
+
+ # Print out.
+ print "HTML:\n";
+ print encode_utf8($tags->flush);
+ print "\n\n";
+ print "CSS:\n";
+ print $css->flush;
+
+ # Output:
+ # HTML:
+ # <table class="info-box">
+ #   <tr>
+ #     <td />
+ #     <td>
+ #       Nábřeží Rudoarmějců
+ #     </td>
+ #   </tr>
+ #   <tr>
+ #     <td />
+ #     <td>
+ #       Příbor
+ #     </td>
+ #   </tr>
+ #   <tr>
+ #     <td />
+ #     <td>
+ #       Česká republika
+ #     </td>
+ #   </tr>
+ # </table>
+ # 
+ # CSS:
+ # .info-box {
+ #         background-color: #32a4a8;
+ #         padding: 1em;
+ # }
+ # .info-box .icon {
+ #         text-align: center;
+ # }
+ # .info-box a {
+ #         text-decoration: none;
+ # }
+
+=head1 DEPENDENCIES
+
+L<Class::Utils>,
+L<Error::Pure>,
+L<Scalar::Util>,
+L<Tags::HTML>.
+
+=head1 REPOSITORY
+
+L<https://github.com/michal-josef-spacek/Tags-HTML-InfoBox>
+
+=head1 AUTHOR
+
+Michal Josef Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+© 2024 Michal Josef Špaček
+
+BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
